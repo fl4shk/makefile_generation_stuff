f1cdd8ccd15f1f04804f2339584296779c6d6bf4
fl4shk <fl4shk@users.noreply.github.com>
2017-05-13 12:30:44 -0500

    First commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+src=makefile_generic_src.gpp
+gpp -DGENERIC ${src} > makefile_generic
+#gpp -DDO_CXX -DHAVE_DISASSEMBLE ${src} > C++/makefile_cxx
+#gpp -DDO_C -DHAVE_DISASSEMBLE ${src} > C/makefile_c
+gpp -DDO_CXX -DHAVE_DEBUG ${src} > C++/makefile_cxx
+gpp -DDO_CXX ${src} > C++/makefile_cxx_no_debug
+gpp -DDO_C -DHAVE_DEBUG ${src} > C/makefile_c
+gpp -DDO_C ${src} > C/makefile_c_no_debug
+gpp -DDO_CXX -DDO_ARM -DHAVE_DEBUG ${src} > C++/makefile_cxx_do_arm
+gpp -DDO_C -DDO_ARM -DHAVE_DEBUG ${src} > C/makefile_c_do_arm
+gpp -DDO_CXX -DDO_ARM -DHAVE_DEBUG -DDO_S -DHAVE_DISASSEMBLE ${src} > C++/makefile_cxx_do_arm_full
+gpp -DDO_C -DDO_ARM -DHAVE_DEBUG -DDO_S -DHAVE_DISASSEMBLE ${src} > C/makefile_c_do_arm_full
