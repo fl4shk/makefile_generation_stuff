73efd33bac0372ec1477a9f734271a9269b85650
fl4shk <fl4shk@users.noreply.github.com>
2017-05-15 11:05:07 -0500

    GBA stuff

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ gpp -DDO_CXX -DHAVE_DEBUG ${src} > C++/makefile_cxx
 gpp -DDO_CXX ${src} > C++/makefile_cxx_no_debug
 gpp -DDO_C -DHAVE_DEBUG ${src} > C/makefile_c
 gpp -DDO_C ${src} > C/makefile_c_no_debug
-gpp -DDO_CXX -DDO_ARM -DHAVE_DEBUG ${src} > C++/makefile_cxx_do_arm
-gpp -DDO_C -DDO_ARM -DHAVE_DEBUG ${src} > C/makefile_c_do_arm
+#gpp -DDO_CXX -DDO_ARM -DHAVE_DEBUG ${src} > C++/makefile_cxx_do_arm
+#gpp -DDO_C -DDO_ARM -DHAVE_DEBUG ${src} > C/makefile_c_do_arm
 gpp -DDO_CXX -DDO_ARM -DHAVE_DEBUG -DDO_S -DHAVE_DISASSEMBLE ${src} > C++/makefile_cxx_do_arm_full
 gpp -DDO_C -DDO_ARM -DHAVE_DEBUG -DDO_S -DHAVE_DISASSEMBLE ${src} > C/makefile_c_do_arm_full
+gpp -DGBA ${src} > C++/makefile_gba
