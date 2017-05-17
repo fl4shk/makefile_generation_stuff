ed5cf0e4bed3524aa5f4130a89b2e16ed0c41cfb
fl4shk <fl4shk@users.noreply.github.com>
2017-05-17 16:18:15 -0500

    Generate makefiles with -DHAVE_DISASSEMBLE

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 src=makefile_generic_src.gpp
 gpp -DGENERIC ${src} > makefile_generic
-#gpp -DDO_CXX -DHAVE_DISASSEMBLE ${src} > C++/makefile_cxx
-#gpp -DDO_C -DHAVE_DISASSEMBLE ${src} > C/makefile_c
 mkdir -p C++
 mkdir -p C
 gpp -DDO_CXX -DHAVE_DEBUG ${src} > C++/makefile_cxx
 gpp -DDO_CXX ${src} > C++/makefile_cxx_no_debug
 gpp -DDO_C -DHAVE_DEBUG ${src} > C/makefile_c
 gpp -DDO_C ${src} > C/makefile_c_no_debug
+gpp -DDO_CXX -DHAVE_DISASSEMBLE ${src} > C++/makefile_cxx_dis
+gpp -DDO_C -DHAVE_DISASSEMBLE ${src} > C/makefile_c_dis
 #gpp -DDO_CXX -DDO_ARM -DHAVE_DEBUG ${src} > C++/makefile_cxx_do_arm
 #gpp -DDO_C -DDO_ARM -DHAVE_DEBUG ${src} > C/makefile_c_do_arm
 gpp -DDO_CXX -DDO_ARM -DHAVE_DEBUG -DDO_S -DHAVE_DISASSEMBLE ${src} > C++/makefile_cxx_do_arm_full
