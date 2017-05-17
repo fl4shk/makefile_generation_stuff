c2c2d812778b458dc9969daa51f1d2b72fc296f9
fl4shk <fl4shk@users.noreply.github.com>
2017-05-17 16:24:00 -0500

    Small changes to build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,16 @@ gpp -DGENERIC ${src} > makefile_generic
 mkdir -p C++
 mkdir -p C
 gpp -DDO_CXX -DHAVE_DEBUG ${src} > C++/makefile_cxx
-gpp -DDO_CXX ${src} > C++/makefile_cxx_no_debug
 gpp -DDO_C -DHAVE_DEBUG ${src} > C/makefile_c
-gpp -DDO_C ${src} > C/makefile_c_no_debug
 gpp -DDO_CXX -DHAVE_DEBUG -DHAVE_DISASSEMBLE ${src} > C++/makefile_cxx_dis
 gpp -DDO_C -DHAVE_DEBUG -DHAVE_DISASSEMBLE ${src} > C/makefile_c_dis
+gpp -DDO_CXX ${src} > C++/makefile_cxx_no_debug
+gpp -DDO_C ${src} > C/makefile_c_no_debug
+
+
 #gpp -DDO_CXX -DDO_ARM -DHAVE_DEBUG ${src} > C++/makefile_cxx_do_arm
 #gpp -DDO_C -DDO_ARM -DHAVE_DEBUG ${src} > C/makefile_c_do_arm
 gpp -DDO_CXX -DDO_ARM -DHAVE_DEBUG -DDO_S -DHAVE_DISASSEMBLE ${src} > C++/makefile_cxx_do_arm_full
 gpp -DDO_C -DDO_ARM -DHAVE_DEBUG -DDO_S -DHAVE_DISASSEMBLE ${src} > C/makefile_c_do_arm_full
+
 gpp -DGBA ${src} > C++/makefile_gba
