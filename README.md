171923d4694b4edf1e4277e90251e2597e134d89
fl4shk <fl4shk@users.noreply.github.com>
2018-05-04 12:03:30 -0400

    Beginning preparations for switching to m4

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Makefile Generation Stuff
 
+<!--
 I've been using this in a bunch of places, so I thought I might as well
 make it public.
 
@@ -13,3 +14,19 @@ tuned for producing .elf files for the ARM7TDMI CPU specifically.
 
 You **could** just manually convert the makefiles, but that's annoying.  It
 would defeat the purpose of makefile\_generic\_src.gpp to do that.
+-->
+
+Generation of GNUmakefiles using GNU m4.
+
+These are indeed specific to the GNU version of Make.
+
+There are some other ones as well that are not generated, such as the
+Icarus Verilog ones, but those are the exception rather than the rule.
+
+## History
+Originally the generation of makefiles was done with
+[GPP](https://logological.org/gpp), the Generic PreProcessor.
+
+After a while (over a year), it was deemed to be unmaintainable, and it was
+decided to switch to GNU m4 for this mini project.
+
---
+++ old_gpp_stuff/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+src=makefile_generic_src.gpp
+mkdir -p generic
+mkdir -p C++
+mkdir -p C
+gpp -DGENERIC ${src} > generic/makefile_generic
+gpp -DANTLR ${src} > generic/makefile_antlr
+gpp -DJSONCPP ${src} > generic/makefile_jsoncpp
+gpp -DANTLR -DJSONCPP ${src} > generic/makefile_antlr_jsoncpp
+
+gpp -DDO_CXX -DHAVE_DEBUG ${src} > C++/makefile_cxx
+gpp -DDO_C -DHAVE_DEBUG ${src} > C/makefile_c
+
+gpp -DDO_CXX -DHAVE_DEBUG -DHAVE_DISASSEMBLE ${src} > C++/makefile_cxx_dis
+gpp -DDO_C -DHAVE_DEBUG -DHAVE_DISASSEMBLE ${src} > C/makefile_c_dis
+
+gpp -DDO_CXX ${src} > C++/makefile_cxx_no_debug
+gpp -DDO_C ${src} > C/makefile_c_no_debug
+
+
+#gpp -DDO_CXX -DDO_ARM -DHAVE_DEBUG ${src} > C++/makefile_cxx_do_arm
+#gpp -DDO_C -DDO_ARM -DHAVE_DEBUG ${src} > C/makefile_c_do_arm
+gpp -DDO_CXX -DDO_ARM -DHAVE_DEBUG -DDO_S -DHAVE_DISASSEMBLE ${src} > C++/makefile_cxx_do_arm_full
+gpp -DDO_C -DDO_ARM -DHAVE_DEBUG -DDO_S -DHAVE_DISASSEMBLE ${src} > C/makefile_c_do_arm_full
+
+gpp -DDO_CXX -DDO_MIPS -DINITIAL_EMBEDDED_DEFINES ${src} > C++/makefile_cxx_do_mips_full
+gpp -DDO_C -DDO_MIPS -DINITIAL_EMBEDDED_DEFINES ${src} > C/makefile_c_do_mips_full
+
+gpp -DGBA ${src} > C++/makefile_gba
+
