86e43a4d788e37ef116dcdb379e9007491261a2a
fl4shk <fl4shk@users.noreply.github.com>
2018-11-09 09:08:08 -0500

    stuffs

--- build_makefiles.py
+++ build_makefiles.py
@@ -16,27 +16,46 @@ def sconcat(*args):
 	return ret
 
 class SrcType(enum.Enum):
-	GENERIC = enum.auto()
-	CXX = enum.auto()
+	Generic = enum.auto()
+	Cxx = enum.auto()
 	C = enum.auto()
 	S = enum.auto()
-	NS = enum.auto()
+	Ns = enum.auto()
 
 class Have(enum.Enum):
-	DEBUG = enum.auto()
-	DISASSEMBLE = enum.auto()
+	Debug = enum.auto()
+	Disassemble = enum.auto()
+	OnlyPreprocess = enum.auto()
 
+class StatusAntlrJsoncpp(enum.Enum):
+	Antlr = enum.auto()
+	Jsoncpp = enum.auto()
+
+class Target(enum.Enum):
+	Host = enum.auto()
+	Arm = enum.auto()
+	Gba = enum.auto()
 
 
 class MakefileBuilder:
-	def __init__(self, filename, src_types, haves, antlr_jsoncpp_status,
-		target):
+	def __init__(self, filename, src_types, haves={Have.Debug},
+		target=Target.Host,
+		status_antlr_jsoncpp={}):
 
 		self.__filename = filename
-		self.__src_types = src_types
-		self.__haves = haves
-		self.__antlr_jsoncpp_status = antlr_jsoncpp_status
-		self.__target = target
+
+		if (src_types == {SrcType.Generic}):
+			self.__src_types = {SrcType.Cxx, SrcType.C, SrcType.S,
+				SrcType.Ns}
+			self.__haves = {Have.Debug, Have.Disassemble,
+				Have.OnlyPreprocess}
+			self.__target = Target.Host
+		else:
+			self.__src_types = src_types
+			self.__haves = haves
+			self.__target = target
+
+		self.__status_antlr_jsoncpp = status_antlr_jsoncpp
 
 		#for st in self.__src_types:
 		#	if ((st != "CXX") and (st != "C") and (st != "S")
@@ -46,7 +65,24 @@ class MakefileBuilder:
 	def build(self):
 		pass
 
+builders \
+= [ \
+	MakefileBuilder("generic/GNUmakefile_generic.mk", {SrcType.Generic}),
+	MakefileBuilder("generic/GNUmakefile_antlr.mk", {SrcType.Cxx},
+		{Have.Debug, Have.Disassemble}, Target.Host,
+		{StatusAntlrJsoncpp.Antlr}),
+	MakefileBuilder("generic/GNUmakefile_jsoncpp.mk", {SrcType.Cxx},
+		{Have.Debug, Have.Disassemble}, Target.Host,
+		StatusAntlrJsoncpp.Jsoncpp),
+	MakefileBuilder("generic/GNUmakefile_antlr_jsoncpp.mk", {SrcType.Cxx},
+		{Have.Debug, Have.Disassemble}, Target.Host,
+		{StatusAntlrJsoncpp.Antlr, StatusAntlrJsoncpp.Jsoncpp}),
 
-#MakefileBuilder("C++/GNUmakefile_cxx.mk", ["CXX"], []).build()
-#MakefileBuilder("C++/GNUmakefile_cxx_dis.mk", ["CXX"], ["DIS"]).build()
+	MakefileBuilder("C++/GNUmakefile_cxx.mk", {SrcType.Cxx}, {Have.Debug}),
+	MakefileBuilder("C++/GNUmakefile_cxx_dis.mk", {SrcType.Cxx},
+		{Have.Debug, Have.Disassemble}),
+]
+
+for builder in builders:
+	builder.build()
 
